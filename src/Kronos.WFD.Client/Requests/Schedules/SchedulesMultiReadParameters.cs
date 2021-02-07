50dc50d9c6ece5696424294fe5da4007764c1eca
Kriti Gupta <Kriti.Gupta@unifiservice.com>
2021-02-07 14:56:05 -0500

    Added a new feature - scheduling

---
+++ src/Kronos.WFD.Client/Requests/Schedules/SchedulesMultiReadParameters.cs
@@ -0,0 +1,67 @@
+﻿using Kronos.WFD.Client.Requests;
+using Kronos.WFD.Client.Serialization;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Kronos.WFD.Client.Requests
+{
+    public class SchedulesMultiReadParameters
+    {
+        [JsonProperty(PropertyName = "multiReadOptions", NullValueHandling = NullValueHandling.Ignore)]
+        public MultiReadOptions MultiReadOptions { get; set; }
+
+        [JsonProperty(PropertyName = "orderBy", NullValueHandling = NullValueHandling.Ignore)]
+        public IEnumerable<string> OrderBy { get; set; }
+
+        [JsonProperty(PropertyName = "select", NullValueHandling = NullValueHandling.Ignore)]
+        public IEnumerable<string> Select { get; set; }
+
+        [JsonProperty(PropertyName = "where", NullValueHandling = NullValueHandling.Ignore)]
+        public SchedulesWhereClause Where { get; set; }
+    }
+    public class SchedulesWhereClause
+    {
+        [JsonProperty(PropertyName = "employees", NullValueHandling = NullValueHandling.Ignore)]
+        public QueryableEmployee Employees { get; set; }
+
+        [JsonProperty(PropertyName = "hyperFind", NullValueHandling = NullValueHandling.Ignore)]
+        public HyperfindQueryParameters HyperFind { get; set; }
+
+        [JsonProperty(PropertyName = "locations", NullValueHandling = NullValueHandling.Ignore)]
+        public LocationQueryParameters Location { get; set; }
+    }
+    public class HyperfindQueryParameters
+    {
+        [JsonProperty(PropertyName ="dateRange", NullValueHandling = NullValueHandling.Ignore)]
+        public QueryableDateRange DateRange { get; set; }
+
+        [JsonProperty(PropertyName = "hyperFind", NullValueHandling = NullValueHandling.Ignore)]
+        public HyperfindLookupParameters HyperFind { get; set; }
+    }
+    public class LocationQueryParameters
+    {
+        [JsonProperty(PropertyName = "dateRange", NullValueHandling = NullValueHandling.Ignore)]
+        public QueryableDateRange DateRange { get; set; }
+
+        [JsonProperty(PropertyName = "locations", NullValueHandling = NullValueHandling.Ignore)]
+        public QueryableLocation Locations {get; set;}
+    }
+
+    public class QueryableLocation
+{
+        [JsonProperty(PropertyName ="ids", NullValueHandling=NullValueHandling.Ignore)]
+        public IEnumerable<string> Ids { get; set; }
+
+        [JsonProperty(PropertyName ="qualifiers", NullValueHandling=NullValueHandling.Ignore)]
+        public IEnumerable<string> qualifiers { get; set; }
+    }
+    public class QueryableEmployee
+    {
+        [JsonProperty(PropertyName = "dateRange", NullValueHandling = NullValueHandling.Ignore)]
+        public QueryableDateRange DateRange { get; set; }
+
+        [JsonProperty(PropertyName = "employees", NullValueHandling = NullValueHandling.Ignore)]
+        public EmployeesQueryParameters Employees { get; set; }
+    }
+}
---
+++ src/Kronos.WFD.Core/Schedule.cs
@@ -0,0 +1,46 @@
+﻿using Kronos.WFD;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kronos.WFD
+{
+    public class Schedule                                                       
+    {                              
+        public IEnumerable<ScheduleShifts> Shifts;                             
+    }
+
+    public class ScheduleShifts
+    {
+        public bool Posted { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public bool SelfServiced { get; set; }
+        public IEnumerable<ScheduleSegments> Segments { get; set; }
+        public bool PostedNotified { get; set; }
+        public bool PostedNotificationPending { get; set; }
+        public bool Deleted { get; set; }
+        public bool Open { get; set; }
+        public bool Locked { get; set; }
+        public string Label { get; set; }
+        public string Id { get; set; }
+        public bool Generated { get; set; }
+        public string EndDateTime { get; set; }
+        public Employee Employee { get; set; }
+    }
+
+    public class ScheduleSegments
+    {
+        public bool UserEnteredWorkrule { get; set; }
+        public bool UserEnteredOrgJob { get; set; }
+        public bool UserEnteredCostCenter { get; set; }
+        public bool TransferWorkrule { get; set; }
+        public string TransferString { get; set; }
+        public bool TransferOrgJob;
+        public bool TransferLaborCategories { get; set; }
+        public bool TransferCostCategories { get; set; }
+        public string StartDateTime { get; set; }
+        public EntityReference OrgJobRef { get; set; }
+        public string Id { get; set; }
+        public string EndDateTime { get; set; }
+    }
+}
