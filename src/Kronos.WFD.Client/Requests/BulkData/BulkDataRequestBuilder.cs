3a156d9955bc1c02db01cbb3e9ef34df52e02a39
Kriti Gupta <Kriti.Gupta@unifiservice.com>
2021-09-03 14:11:22 -0400

    Feature added to collect bulk data

---
+++ src/Kronos.WFD.Client/Requests/BulkData/BulkDataRequestBuilder.cs
@@ -0,0 +1,21 @@
+﻿namespace Kronos.WFD.Client.Requests.BulkData
+{
+    public class BulkDataRequestBuilder : BaseRequestBuilder, IBulkDataRequestBuilder
+    {
+        public BulkDataRequestBuilder(string requestUrl, IBaseClient client) : base(requestUrl, client)
+        {
+
+        }
+        public IBulkDataCollectionRequestBuilder BulkDataAsync
+        {
+            get
+            {
+                return new BulkDataCollectionRequestBuilder(this.AppendSegmentToRequestUrl("exports"), this.Client);
+            }
+        }
+    }
+    public interface IBulkDataRequestBuilder
+    {
+            IBulkDataCollectionRequestBuilder BulkDataAsync { get; }
+     }
+}
